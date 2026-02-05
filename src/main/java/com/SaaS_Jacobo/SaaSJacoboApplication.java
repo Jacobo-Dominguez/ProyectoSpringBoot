949526964ad008130460bdd3c9d26acb4e7d484e
Jacobo-Dominguez <jacobo9810@gmail.com>
2026-02-05 16:52:50 +0100

    Creacion del proyecto

---
+++ src/main/java/com/SaaS_Jacobo/SaaSJacoboApplication.java
@@ -0,0 +1,13 @@
+package com.SaaS_Jacobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSJacoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaSJacoboApplication.class, args);
+	}
+
+}
